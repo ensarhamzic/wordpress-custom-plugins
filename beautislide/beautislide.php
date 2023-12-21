4acb8ab80cbb203f7608a633778e7256241c8bba
EnsarHamzic <ensar.hamzic@bitconex.de>
2023-12-21 22:07:03 +0100

    enqueue styles to plugin

--- beautislide/beautislide.php
+++ beautislide/beautislide.php
@@ -13,5 +13,23 @@
  * @package Beautislide
  */
 
+ if (!function_exists('add_action')) {
+    echo 'Hi there! I\'m just a plugin, not much I can do when called directly.';
+    exit;
+}
+
+function beautislide_scripts() {
+
+  $plugin_directory_url = plugin_dir_url( __FILE__ );
+   	// Flexslider Javascript and CSS files
+	wp_enqueue_script( 'flexslider-min-js', $plugin_directory_url . '/inc/flexslider/jquery.flexslider-min.js', array( 'jquery' ), '', true );
+	wp_enqueue_style( 'flexslider-css', $plugin_directory_url . '/inc/flexslider/flexslider.css', array(), '', 'all' );
+	wp_enqueue_script( 'flexslider-js', $plugin_directory_url . '/inc/flexslider/flexslider.js', array( 'jquery' ), '', true );
+
+  wp_enqueue_style('flexslider-custom-css', $plugin_directory_url . '/inc/style.css', array(), '', 'all');
+}
+
+add_action( 'wp_enqueue_scripts', 'beautislide_scripts' );
+
 
  ?>

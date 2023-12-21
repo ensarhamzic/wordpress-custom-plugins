96105f39cc633864a114b1bfe07055ffcdbca7ef
EnsarHamzic <ensar.hamzic@bitconex.de>
2023-12-21 22:06:25 +0100

    add main beautislide file, add plugin details

---
+++ beautislide/beautislide.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Beautislide
+ * Plugin URI: https://beautislide.com/
+ * Description: Very beautiful slider for your website.
+ * Version: 1.0.0
+ * Author: Ensar Hamzic
+ * Author URI: https://ensarhamzic.com
+ * Text Domain: ensarhamzic
+ * Requires at least: 6.3
+ * Requires PHP: 7.4
+ *
+ * @package Beautislide
+ */
+
+
+ ?>

4abb3ecc11af49f0e78498cc4617f99f73e8265c
EnsarHamzic <ensar.hamzic@bitconex.de>
2023-12-23 18:44:21 +0100

    woohome - add basic plugin info

---
+++ woohome/woohome.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Woohome
+ * Plugin URI: https://woohome.com/
+ * Description: Very beautiful slider for your website.
+ * Version: 1.0.0
+ * Author: Ensar Hamzic
+ * Author URI: https://ensarhamzic.com
+ * Text Domain: ensarhamzic
+ * Requires at least: 6.3
+ * Requires PHP: 7.4
+ *
+ * @package Woohome
+ */
